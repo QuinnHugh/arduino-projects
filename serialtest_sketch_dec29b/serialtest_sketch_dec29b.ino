1f455779759275f33fc7af7aec875faa6b0f257f
Kun <quinnHugh@hotmail.com>
2019-07-28 07:45:43 +0800

    update

--- serialtest_sketch_dec29b/serialtest_sketch_dec29b.ino
+++ serialtest_sketch_dec29b/serialtest_sketch_dec29b.ino
@@ -1,7 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); //set up serial library baud rate to 9600
-  Serial.println("hello world");
+  Serial.println("hello worldhzhzhz");
 }
 
 void loop() {
